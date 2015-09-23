7d8fc075fbe3d431d5e171f25dbe07b6de8928e2
Mariana Dates <mda@softworld.dk>
2015-09-23 17:13:05 +0200

    Added the files for connection withthe database

---
+++ src/main/java/dao/DBConnect.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package dao;
+
+import java.sql.*;
+
+/**
+ * @author Mariana
+ *
+ */
+public class DBConnect {
+private static Connection con = null;
+	
+	private DBConnect(){		
+	}
+	public static Connection getConnection(){		
+		//If instance has not been created yet, create it
+		if(DBConnect.con == null){
+			initConnection();
+		}
+		return DBConnect.con;
+	}
+	private static void initConnection(){			
+		try{		
+			Class.forName("com.mysql.jdbc.Driver");		
+			
+			String url = "jdbc:mysql://localhost:3306/ExamHelper";
+			String user = "ExamUser";
+			String pw = "ExamHelperPass";
+			//I have used some fiction username and password today, maybe tomorrow we can make to be the same on each computer
+			DBConnect.con = DriverManager.getConnection(url, user, pw);		
+		}
+		catch (ClassNotFoundException e){		
+			System.out.println("class not found " + e.getMessage());
+			System.exit(0);
+		}
+		catch (SQLException e){			
+			System.out.println("sql exception " + e.getMessage());
+			System.exit(0);
+		}
+		catch (Exception e){	
+			System.out.println("exception " +e.getMessage());
+		}		
+	}
+}
